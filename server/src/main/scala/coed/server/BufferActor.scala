77d0bcf49b0b232207d52405c99e8e5996256b1b
Daniel Kelencz <daniel.kelencz@balabit.com>
2017-12-14 11:07:47 +0100

    serveractor: add loadBuffer method

--- server/src/main/scala/coed/server/BufferActor.scala
+++ server/src/main/scala/coed/server/BufferActor.scala
@@ -4,9 +4,24 @@ import akka.actor.Actor
 import coed.common.Protocol._
 import coed.common.{Buffer, StringBuf}
 
-class BufferActor(text: String) extends Actor {
+import scala.io.Source
+import scala.util.{Failure, Success, Try}
 
-  var buffer: Buffer = new StringBuf(text)
+class BufferActor(filename: String) extends Actor {
+
+  var buffer: Buffer = new StringBuf(loadBuffer)
+
+  private def loadBuffer: String = {
+    val fileHandle = Try { Source.fromFile(filename) }
+    val returnValue = fileHandle.map(_.mkString).getOrElse("")
+
+    fileHandle match {
+      case Success(fh) => fh.close()
+      case Failure(_) => Unit
+    }
+
+    returnValue
+  }
 
   override def receive = {
     case Open(_) =>
