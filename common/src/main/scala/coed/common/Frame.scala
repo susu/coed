5b30e87dcbdc2aa61c36971c5cdc78bb2ffbf665
Imre Gulyas <imre.gulyas@balabit.com>
2017-12-15 16:32:04 +0100

    add cursor movement to frame
    todo: vertical movement is not perfect
    
    Signed-off-by: Imre Gulyas <imre.gulyas@balabit.com>

--- common/src/main/scala/coed/common/Frame.scala
+++ common/src/main/scala/coed/common/Frame.scala
@@ -1,21 +1,75 @@
 package coed.common
 
-case class FrameCoords(line: Int, at: Int) // indexing starts at 1
+case class FrameCoords(at: Int, line: Int) {
+  require(at > 0)
+  require(line > 0)
+}
 
+/** Class to represent the displayed rectangular section of a buffer.
+  *
+  * @param bufferText  text of buffer to display in frame
+  * @param bufferOffset coordinates of top left corner of frame
+  * @param cursorPosition cursor position in frame coordinates
+  * @param frameWidth
+  * @param frameHeight
+  */
 case class Frame(bufferText: String,
                  bufferOffset: (Int, Int) = (0, 0),  // x and y offsets
                  cursorPosition: FrameCoords = FrameCoords(1, 1), // indexing starts from 1, 1
                  frameWidth: Int = Frame.DEFAULT_FRAME_WIDTH,
                  frameHeight: Int = Frame.DEFAULT_FRAME_HEIGHT) {
 
+  require(bufferOffset._1 >= 0)
+  require(bufferOffset._2 >= 0)
+  require(frameWidth > 0)
+  require(frameHeight > 0)
+  require(cursorPosition.line > 0 && cursorPosition.line <= frameHeight)
+  require(cursorPosition.at > 0 && cursorPosition.at <= frameWidth)
+
   private val linesInBuffer: Vector[String] = bufferText.lines.toVector
 
   val visibleLines: Seq[String] = (1 to frameHeight).map { calculateVisibleLine(_) }
 
-  def moveCursorUp: Frame = ???
-  def moveCursorDown: Frame = ???
-  def moveCursorLeft: Frame = ???
-  def moveCursorRight: Frame = ???
+  def moveCursorUp: Frame = {
+    if (cursorPosition.line == 1) {
+      if (bufferOffset._2 == 0)  this
+      else this.copy(bufferOffset = (this.bufferOffset._1, this.bufferOffset._2 - 1))
+    } else {
+      this.copy(cursorPosition = FrameCoords(this.cursorPosition.at, this.cursorPosition.line - 1))
+    }
+  }
+
+  def moveCursorDown: Frame = {
+    if (cursorPosition.line == frameHeight) {
+      if (bufferOffset._2 == linesInBuffer.size - frameHeight) this
+      else this.copy(bufferOffset = (this.bufferOffset._1, this.bufferOffset._2 + 1))
+    } else {
+      this.copy(cursorPosition = FrameCoords(this.cursorPosition.at, this.cursorPosition.line + 1))
+    }
+  }
+
+  def moveCursorLeft: Frame = {
+    if (cursorPosition.at == 1) {
+      if (bufferOffset._1 == 0)  this
+      else this.copy(bufferOffset = (this.bufferOffset._1 - 1, this.bufferOffset._2))
+    } else {
+      this.copy(cursorPosition = FrameCoords(this.cursorPosition.at - 1, this.cursorPosition.line))
+    }
+  }
+
+  def moveCursorRight: Frame = {
+    val currentLineLength = linesInBuffer(cursorPosition.line - 1).size
+    if (cursorPosition.at == frameWidth) { // cursor is at right edge of frame
+        if (bufferOffset._1 == currentLineLength - frameWidth) this
+        else this.copy(bufferOffset = (this.bufferOffset._1 + 1, this.bufferOffset._2))
+    } else {
+        if (bufferOffset._1 + cursorPosition.at == currentLineLength) { //mid of frame, end of line
+          this
+        } else  {
+          this.copy(cursorPosition = FrameCoords(this.cursorPosition.at + 1, this.cursorPosition.line))
+        }
+    }
+  }
 
   private def calculateVisibleLine(lineIndexInFrame: Int): String = {
     val fullLineFromBuffer = translateBufferLineToFrameLine(lineIndexInFrame)
--- common/src/test/scala/coed/common/FrameSuite.scala
+++ common/src/test/scala/coed/common/FrameSuite.scala
@@ -3,12 +3,11 @@ package coed.common
 import org.scalatest.{FreeSpec, Matchers}
 
 class FrameSuite extends FreeSpec with Matchers {
-  val testBufferText: String = (
-    "123456789\n" +
-    "23\n" +
-    "3456789\n" +
-    "456789101112\n" +
-    "56789\n")
+  val testBufferText: String =  "123456789\n" +
+                                "23\n" +
+                                "3456789\n" +
+                                "456789101112\n" +
+                                "56789\n"
 
   "calculating visible lines in buffer" - {
     "when offset is (0, 0) and whole frame fits in buffer" in {
@@ -45,6 +44,145 @@ class FrameSuite extends FreeSpec with Matchers {
       val expectedLines = Seq("1", "2", "3", "4", "5", Frame.EMPTY_LINE_REPRESENTATION)
 
       testFrame.visibleLines shouldBe expectedLines
+    }
+  }
+
+  "cursor movement" - {
+    "up" - {
+      "should return same frame when [Y buffer offset: 0, Y cursor pos: 1 (top of frame)]" in {
+        val frame = Frame (bufferText = testBufferText, bufferOffset = (3, 0), cursorPosition = FrameCoords(3, 1))
+        val newFrame = frame.moveCursorUp
+        newFrame shouldBe frame
+      }
+
+      "only change should be 1 less Y buffer offset when [Y buffer offset: >0, Y cursor pos: 1]" in {
+        val frame = Frame (bufferText = testBufferText, bufferOffset = (3, 1), cursorPosition = FrameCoords(3, 1))
+        val newFrame = frame.moveCursorUp
+        newFrame.bufferOffset._1 shouldBe frame.bufferOffset._1
+        newFrame.bufferOffset._2 shouldBe frame.bufferOffset._2 - 1
+        newFrame.cursorPosition shouldBe frame.cursorPosition
+      }
+
+      "only change should be 1 less Y cursor pos when [Y buffer offset: >0, Y cursor pos: >1]" in {
+        val frame = Frame (bufferText = testBufferText, bufferOffset = (3, 1), cursorPosition = FrameCoords(1, 2))
+        val newFrame = frame.moveCursorUp
+        newFrame.bufferOffset shouldBe frame.bufferOffset
+        newFrame.cursorPosition.at shouldBe frame.cursorPosition.at
+        newFrame.cursorPosition.line shouldBe frame.cursorPosition.line - 1
+      }
+    }
+
+    "left" - {
+      "should return same frame when [X buffer offset: 0, X cursor pos: 1 (left side of frame)]" in {
+        val frame = Frame (bufferText = testBufferText, bufferOffset = (0, 3), cursorPosition = FrameCoords(1, 3))
+        val newFrame = frame.moveCursorLeft
+        newFrame shouldBe frame
+      }
+
+      "only change should be 1 less X buffer offset when [X buffer offset: >0, X cursor pos: 1]" in {
+        val frame = Frame (bufferText = testBufferText, bufferOffset = (1, 3), cursorPosition = FrameCoords(1, 3))
+        val newFrame = frame.moveCursorLeft
+        newFrame.bufferOffset._1 shouldBe frame.bufferOffset._1 - 1
+        newFrame.bufferOffset._2 shouldBe frame.bufferOffset._2
+        newFrame.cursorPosition shouldBe frame.cursorPosition
+      }
+
+      "only change should be 1 less X cursor pos when [X buffer offset: >0, X cursor pos: >1]" in {
+        val frame = Frame (bufferText = testBufferText, bufferOffset = (1, 3), cursorPosition = FrameCoords(2, 2))
+        val newFrame = frame.moveCursorLeft
+        newFrame.bufferOffset shouldBe frame.bufferOffset
+        newFrame.cursorPosition.at shouldBe frame.cursorPosition.at - 1
+        newFrame.cursorPosition.line shouldBe frame.cursorPosition.line
+      }
+    }
+
+    "down" - {
+      "should return same frame when [Y buffer offset: buffer height - frame height, Y cursor pos: frame height]" in {
+        val testBufferHeight = testBufferText.lines.size
+        val testFrameHeight = 3
+        val frame = Frame (bufferText = testBufferText,
+                           frameHeight = testFrameHeight,
+                           bufferOffset = (3, testBufferHeight - testFrameHeight),
+                           cursorPosition = FrameCoords(3, testFrameHeight))
+
+        val newFrame = frame.moveCursorDown
+        newFrame shouldBe frame
+      }
+
+      "only change should be 1 more Y buffer offset when [Y buffer offset: > (buffer height - frame height), Y cursor pos: frame height]" in {
+        val testBufferHeight = testBufferText.lines.size
+        val testFrameHeight = 3
+        val frame = Frame (bufferText = testBufferText,
+                           frameHeight = testFrameHeight,
+                           bufferOffset = (3, (testBufferHeight - testFrameHeight) - 1),
+                           cursorPosition = FrameCoords(3, testFrameHeight))
+
+        val newFrame = frame.moveCursorDown
+        newFrame.bufferOffset._1 shouldBe frame.bufferOffset._1
+        newFrame.bufferOffset._2 shouldBe frame.bufferOffset._2 + 1
+        newFrame.cursorPosition shouldBe frame.cursorPosition
+      }
+
+      "only change should be 1 more Y cursor pos when [Y cursor pos: < frame height]" in {
+        val frame = Frame (bufferText = testBufferText, bufferOffset = (3, 1), cursorPosition = FrameCoords(1, 2))
+        val newFrame = frame.moveCursorDown
+        newFrame.bufferOffset shouldBe frame.bufferOffset
+        newFrame.cursorPosition.at shouldBe frame.cursorPosition.at
+        newFrame.cursorPosition.line shouldBe frame.cursorPosition.line + 1
+      }
+    }
+  }
+
+  "right" - {
+    val testText: String = "123456789\n" +
+                           "2345678"
+
+    val testFrameWidth = 3
+    val currentLineNumberInFrameCoords = 2
+    val currentLine = testText.lines.toList(currentLineNumberInFrameCoords - 1)
+
+    "should return same frame when [current line length: X buffer offset + X cursor position, X cursor pos: frame width]" in {
+      val frame = Frame (bufferText = testText,
+        frameWidth = testFrameWidth,
+        bufferOffset = (currentLine.size - testFrameWidth, 0),
+        cursorPosition = FrameCoords(testFrameWidth, currentLineNumberInFrameCoords))
+
+      val newFrame = frame.moveCursorRight
+      newFrame shouldBe frame
+    }
+
+    "only change should be 1 more X buffer offset when [current line length: > (X buffer offset + X cursor position), X cursor pos: frame height]" in {
+      val frame = Frame (bufferText = testText,
+        frameWidth = testFrameWidth,
+        bufferOffset = (currentLine.size - testFrameWidth - 1, 0),
+        cursorPosition = FrameCoords(testFrameWidth, currentLineNumberInFrameCoords))
+
+      val newFrame = frame.moveCursorRight
+      newFrame.bufferOffset._1 shouldBe frame.bufferOffset._1 + 1
+      newFrame.bufferOffset._2 shouldBe frame.bufferOffset._2
+      newFrame.cursorPosition shouldBe frame.cursorPosition
+    }
+
+    "cursor is in middle of frame and visible line is shorter than frame width and cursor is at the end of line" in {
+      val txt = "new\n" +
+                "ne\n" +
+                "new"
+
+      val frame = Frame (bufferText = txt, bufferOffset = (0, 0), cursorPosition = FrameCoords(2, 2))
+      val newFrame = frame.moveCursorRight
+      newFrame shouldBe frame
+    }
+
+    "cursor is in middle of frame and visible line is shorter than frame width and cursor is not at the end of line" in {
+      val txt = "new\n" +
+                "ne\n" +
+                "new"
+
+      val frame = Frame (bufferText = txt, bufferOffset = (0, 0), cursorPosition = FrameCoords(1, 2))
+      val newFrame = frame.moveCursorRight
+      newFrame.bufferOffset shouldBe frame.bufferOffset
+      newFrame.cursorPosition.at shouldBe frame.cursorPosition.at + 1
+      newFrame.cursorPosition.line shouldBe frame.cursorPosition.line
     }
   }
 }
