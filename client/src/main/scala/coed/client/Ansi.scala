5685ed53b380cce8dadcbea7ee3daac7653b2877
Marton Suranyi <marton.suranyi@balabit.com>
2017-12-18 16:15:30 +0100

    more fancy stuff: colors, linenumbers

--- client/src/main/scala/coed/client/Ansi.scala
+++ client/src/main/scala/coed/client/Ansi.scala
@@ -3,4 +3,9 @@ package coed.client
 object Ansi {
   val clearScreenCode: String = moveCursorCode(0, 0) ++ "\u001B[2J"
   def moveCursorCode(x: Int, y: Int): String = s"\u001B[${y};${x}H"
+
+  def colorForeground(fg: Int): String = s"\u001B[38;5;${fg}m"
+  def colorBackground(bg: Int): String = s"\u001B[48;5;${bg}m"
+
+  val resetColor: String = "\u001B[0m"
 }
--- client/src/main/scala/coed/client/BufferRenderer.scala
+++ client/src/main/scala/coed/client/BufferRenderer.scala
@@ -1,20 +1,50 @@
 package coed.client
 
+import java.io.PrintStream
+
 import coed.common.Frame
 
+class BufferedWriter(output: PrintStream) {
+  val buf: StringBuilder = new StringBuilder
+  def append(s: String): Unit = buf.append(s)
+  def flush(): Unit = output.print(buf)
+}
+
 object BufferRenderer {
+
   def show(frame: Frame): Unit = {
-    print(Ansi.clearScreenCode)
-    frame.visibleLines.foreach { line => println(line + "\r") }
-    print(Ansi.moveCursorCode(frame.cursorPosition.at, frame.cursorPosition.line))
+    val output = new BufferedWriter(Console.out)
+
+    output.append(Ansi.clearScreenCode)
+    val indexFrom = frame.bufferOffset._2
+    val lineNumbers = indexFrom to (indexFrom + Frame.DEFAULT_FRAME_HEIGHT)
+
+    def formatLineNumber(number: Int) =
+      s"${Ansi.colorForeground(3)}${leftpad(number)}${Ansi.resetColor}"
+
+    frame.visibleLines.zip(lineNumbers).foreach {
+      case (line, number) => output.append(s"${formatLineNumber(number)} $line\n\r")
+    }
+    output.append(Ansi.moveCursorCode(frame.cursorPosition.at + 4, frame.cursorPosition.line))
+    output.flush()
   }
 
   def showAlternateBuffer(text: String): Unit = {
-    print(Ansi.moveCursorCode(1, Frame.DEFAULT_FRAME_HEIGHT + 1))
-    print("--------------------------------------")
-    print(Ansi.moveCursorCode(1, Frame.DEFAULT_FRAME_HEIGHT + 2))
-    text.lines.foreach(line => print(line++"\n\r"))
-    print("--------------------------------------")
+    val output = new BufferedWriter(Console.out)
+    val decoratorLine = Ansi.colorForeground(5) +
+      (0 to Frame.DEFAULT_FRAME_WIDTH).map(_ => "-").mkString("") +
+      Ansi.resetColor + "\r\n"
+
+    output.append(Ansi.moveCursorCode(1, Frame.DEFAULT_FRAME_HEIGHT + 1))
+    output.append(decoratorLine)
+    output.append(Ansi.moveCursorCode(1, Frame.DEFAULT_FRAME_HEIGHT + 2))
+    text.lines.foreach(line => output.append(line++"\n\r"))
+    output.append(decoratorLine)
+    output.flush()
+  }
+
+  def leftpad(number: Int): String = {
+    f"$number%03d"
   }
 }
 
